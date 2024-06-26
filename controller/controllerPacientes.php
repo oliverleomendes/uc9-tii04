cd343554d264b52b64662e3d1c9cafb67512fe4a
oliverleomendes <leo.jodo@gmail.com>
2024-06-25 21:48:00 -0300

    Aula 25.06.2024

---
+++ controller/controllerPacientes.php
@@ -0,0 +1,15 @@
+<?php
+
+class controllerPacientes {
+
+    public function cadastrarPaciente($nome, $sobrenome, $email, $cep, $logradouro,
+                                        $numero, $bairro, $cidade, $uf){
+        try {
+            $modelPacientes = new modelPacientes();
+            return $modelPacientes->cadastrarPaciente($nome, $sobrenome, $email, $cep, $logradouro,
+                                                        $numero, $bairro, $cidade, $uf);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+}
